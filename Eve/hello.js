717993b2bc6f9c2da34a70d8f3b4f0dd4e183637
sumkit <skitahar@andrew.cmu.edu>
2016-11-16 16:27:04 -0500

    Eve. URL test

---
+++ Eve/hello.js
@@ -0,0 +1,169 @@
+// Your Client ID can be retrieved from your project in the Google
+// Developer Console, https://console.developers.google.com
+var CLIENT_ID = '571876370007-1peoaj3v39c15glembj915jl4eaib8a2.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyClrfIxWqPqslBjRtKrHi1U6zKJP7Uequk'
+var SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+// var SCOPES = 'https://www.googleapis.com/auth/gmail.readonly';
+
+function handleClientLoad() {
+  gapi.client.setApiKey(API_KEY);
+  window.setTimeout(checkAuth, 1);
+}
+
+/**
+ * Check if current user has authorized this application.
+ */
+function checkAuth() {
+  gapi.auth.authorize(
+    {
+      'client_id': CLIENT_ID,
+      'scope': SCOPES.join(' '),
+      'immediate': true
+    }, handleAuthResult);
+  // gapi.auth.authorize({
+  //   client_id: CLIENT_ID,
+  //   scope: SCOPES,
+  //   immediate: true
+  // }, handleAuthResult);
+}
+
+/**
+ * Handle response from authorization server.
+ *
+ * @param {Object} authResult Authorization result.
+ */
+function handleAuthResult(authResult) {
+  var authorizeDiv = document.getElementById('authorize-div');
+  if (authResult && !authResult.error) {
+    // Hide auth UI, then load client library.
+    authorizeDiv.style.display = 'none';
+    loadGmailApi();
+  } else {
+    // Show auth UI, allowing the user to initiate authorization by
+    // clicking authorize button.
+    authorizeDiv.style.display = 'inline';
+  }
+}
+
+ /**
+ * Print all Labels in the authorized user's inbox. If no labels
+ * are found an appropriate message is printed.
+ */
+function listLabels() {
+  var request = gapi.client.gmail.users.labels.list({
+    'userId': 'me'
+  });
+
+  request.execute(function(resp) {
+    var labels = resp.labels;
+    appendPre('Labels:');
+
+    if (labels && labels.length > 0) {
+      for (i = 0; i < labels.length; i++) {
+        var label = labels[i];
+        appendPre(label.name)
+      }
+    } else {
+      appendPre('No Labels found.');
+    }
+  });
+  }
+
+/**
+ * Initiate auth flow in response to user clicking authorize button.
+ *
+ * @param {Event} event Button click event.
+ */
+function handleAuthClick(event) {
+  gapi.auth.authorize(
+    {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
+    handleAuthResult);
+  return false;
+}
+
+function start() {
+  gapi.client.init({
+    'apiKey': API_KEY,
+    'discoveryDocs': [SCOPES],
+  }).then(function() {
+    gapi.client.load('gmail', 'v1', getInbox);
+    gapi.client.load('gmail', 'v1', getUnread);
+    gapi.client.load('gmail', 'v1', listLabels);
+  }).then(function(response) {
+    console.log(response);
+  }, function(reason) {
+    console.log('Error: ' + reason.result.error.message);
+  });
+};
+
+/**
+ * Load Gmail API client library. List labels once client library
+ * is loaded.
+ */
+function loadGmailApi() {
+  console.log("here")
+  gapi.load('client', start);
+}
+
+/**
+ * Append a pre element to the body containing the given message
+ * as its text node.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
+function appendPre(message) {
+  var pre = document.getElementById('output');
+  var textContent = document.createTextNode(message + '\n');
+  pre.appendChild(textContent);
+}
+
+function getInbox() {
+  var request = gapi.client.gmail.users.messages.list({
+    'userId': 'me',
+    'labelIds': 'INBOX',
+    'maxResults': 10
+  });
+
+  request.execute(function(response) {
+    $.each(response.messages, function() {
+      var messageRequest = gapi.client.gmail.users.messages.get({
+        'userId': 'me',
+        'id': this.id
+      });
+
+      messageRequest.execute(displayMessage);
+    });
+  });
+}
+
+function getUnread() {
+  var request = gapi.client.gmail.users.messages.list({
+    'userId': 'me',
+    'labelIds': 'UNREAD',
+    'maxResults': 30
+  });
+
+  request.execute(function(response) {
+    $.each(response.messages, function() {
+      var messageRequest = gapi.client.gmail.users.messages.get({
+        'userId': 'me',
+        'id': this.id
+      });
+
+      // messageRequest.execute(displayMessage);
+    });
+  });
+} 
+
+function displayMessage(message) {
+  var headers = message.payload.headers;
+  $.each(headers, function() {
+    if(this.name.toLowerCase() === "subject") {
+        console.log(this.value);
+    }
+  })
+  //   console.log(message.snippet);
+
+  // var date = new Date(message.internalDate);
+  // console.log(date.toString("MMM dd"));
+}
