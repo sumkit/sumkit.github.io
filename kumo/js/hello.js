cde6fbb9cc4a3a722f42d4e932c27200b8df72e8
sumkit <skitahar@andrew.cmu.edu>
2016-12-01 10:58:49 -0500

    check for null body data

--- kumo/js/hello.js
+++ kumo/js/hello.js
@@ -168,7 +168,10 @@ function handleInbox(message) {
 
 function displayMessage(message) {
     console.log(message.payload.body);
-    var bodyMsg = atob(message.payload.body.data);
+    var bodyMsg = "";
+    if(message.payload.body.data != null) {
+        bodyMsg = atob(message.payload.body.data);
+    } 
     
   var headers = message.payload.headers;
   var paper = Raphael(windowWidth/8, windowHeight/3, windowWidth, (5/8)*windowHeight);
