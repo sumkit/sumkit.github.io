d5cbb078a129c600356f08e23029d775286e913c
sumkit <skitahar@andrew.cmu.edu>
2016-12-03 01:58:32 -0500

    auth plz work

--- kumo/js/hello.js
+++ kumo/js/hello.js
@@ -109,10 +109,12 @@ function handleAuthClick() {
  * @param {Object} authResult Authorization result.
  */
 function handleAuthResult(authResult) {
+    console.log(authResult);
   var authorizeDiv = document.getElementById('authorize-div');
   if (authResult && !authResult.error) {
     // Hide auth UI, then load client library.
     authorizeDiv.style.display = 'none';
+    gapi.client.load('gmail', 'v1', getInbox);
   } else {
     // Show auth UI, allowing the user to initiate authorization by
     // clicking authorize button.
