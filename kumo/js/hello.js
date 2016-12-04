7f20e2188ed9761197c4226a94c8748a4caa05e5
sumkit <skitahar@andrew.cmu.edu>
2016-12-04 14:19:14 -0500

    rotate envelopes

--- kumo/js/hello.js
+++ kumo/js/hello.js
@@ -300,7 +300,7 @@ function displayMessage(message, tag) {
     // Sets the stroke attribute of the rectangle to green with width 8
     rect.attr("stroke", green);
     rect.attr("stroke-width", "8");
-    
+    rect.rotate(animDelay*2);
     var frontStr = "From: "+from+"\nSubject: "+subject;
     var t = envelopePaper.text(0.75*windowWidth,windowHeight/4, "");
     t.attr("fill", "#000");
@@ -309,6 +309,7 @@ function displayMessage(message, tag) {
     t.attr("font-weight", "normal");
     t.attr("font-family", "arial");
     t.attr("text-anchor", "start");
+    t.rotate(animDelay*2);
     formatText(frontStr, (windowHeight/2)-5, t);
     
     rect.drag(function(dx, dy) {
