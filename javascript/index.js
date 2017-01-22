a7dc9f879d495892f29169dee18507f580b23d36
sumkit <skitahar@andrew.cmu.edu>
2017-01-21 21:49:43 -0500

    minimalist

--- javascript/index.js
+++ javascript/index.js
@@ -1,106 +1,74 @@
-var windowWidth = 200; //init values
-var windowHeight = 200;
+//global variables
+var addedSectionLabel=false;
 
-function hoverText(elem) {
-    var label = document.getElementById("label");
-    label.innerHTML=elem.id;
-    var left = elem.offsetLeft + (elem.width/5);
-    var top = elem.offsetTop;
-    top -= 30;
-    $("#label").offset({ left: left, top: top});
-}
-
-function hideElements() {
-    var divs = document.getElementsByClassName("section");
-    for(var i=0; i<divs.length; i++) {
-        var d = divs[i];
-        d.style.display="none";
-    }
-}
-
-function coffeeRain() {
-    hideElements();
-    var interval = $(window).width()/20;
-    var y1 = $(window).height();
-    var paper = Raphael(0,0,$(window).width(), y1);
-    var anim1 = Raphael.animation({y: y1}, 2000, "linear");
-    for(var i=0; i<20; i++) {
-        var x1 = i*interval;
-        var raphDrop = paper.image("media/desk/drop.png", x1, 0, y1/10, y1/10);
-        if(i==0) {
-            //show about me div
-            var anim2 = Raphael.animation({y: y1}, 2000, "linear", function() {
-                document.getElementById("about-me").style.display="inline"; 
-            });
-            anim2 = anim2.delay(800);
-            raphDrop.animate(anim2);
-        }
-        else if(i==19) {
-            //last drop
-            var anim2 = Raphael.animation({y: y1}, 2000, "linear", function() {
-                paper.remove();
-            });
-            anim2 = anim2.delay(1000);
-            raphDrop.animate(anim2);
+function drawSidebar(width, height) {
+    var space = height/15;
+    var radius = height/80;
+    var sections=["about", "projects", "golf", "résumé", "contact"];
+    var canvases = document.getElementsByClassName('sidebar');
+    for(var i=0;i<canvases.length;i++) {
+        var ctx=canvases[i].getContext("2d");
+        ctx.canvas.width = (radius*3);
+        canvases[i].style.left="90%";
+        if(canvases[i].id=="sidebar-line") {
+            ctx.canvas.height = (5*space);
+            canvases[i].style.top="33%";
+            ctx.moveTo(radius+3, (radius+1));
+            ctx.lineTo(radius+3, (4*space)+radius);
+            
         } else {
-            var rand = Math.random()*10;
-            anim1 = anim1.delay(rand*100);
-            raphDrop.animate(anim1);
+            ctx.canvas.height = (2*radius)+6;
+            var y=((i-1)*space)+(height/3);
+            canvases[i].style.top=y+"px";
+            ctx.beginPath();
+            ctx.arc(radius+3,radius+3,height/75, 0,2*Math.PI);
+            ctx.fillStyle = 'white';
+            ctx.fill();
+            ctx.strokeStyle="#7383BF";
+            ctx.stroke();
+            
+            canvases[i].addEventListener('mousemove', function(event) {
+                if(!addedSectionLabel) {
+                    addedSectionLabel = true;
+                    var id=this.id.split('-')[1];
+                    var pos = $(this).position();
+                    var top = pos.top;
+                    var left = pos.left-(8*id.length)-radius-3;
+                    var p = document.createElement('p');
+                    p.className="temp-label";
+                    p.innerHTML=id;
+                    p.style.position="fixed";
+                    p.style.left=left+"px";
+                    p.style.top=top+"px";
+                    p.style.textAlign = "start";
+                    document.body.appendChild(p);
+                }
+            }, false);
+            canvases[i].addEventListener('mouseleave', function(event) {
+                $("p").remove(".temp-label");
+                addedSectionLabel = false;
+            }, false);
+            canvases[i].addEventListener('click', function(event) {
+                var id=this.id.split('-')[1];
+                console.log(this.hash);
+                $('html, body').animate({
+                    scrollTop: $("#"+id+"-div").offset().top
+                }, 1000);
+            }, false);
         }
+        ctx.strokeStyle="#7383BF";
+        ctx.lineWidth = 3;
+        ctx.stroke();
     }
 }
-
-function changeX(id) {
-    var plusID = id+"_+";
-    var timesID = id+"_x";
-    var imgDiv = id+"_imgs";
-    if(document.getElementById(plusID).style.display == 'none') {
-        document.getElementById(plusID).style.display="inline";
-        document.getElementById(timesID).style.display="none";
-        document.getElementById(imgDiv).style.display="none";
-    } else {
-        document.getElementById(plusID).style.display="none";
-        document.getElementById(timesID).style.display="inline";
-        document.getElementById(imgDiv).style.display="block";
-    }
-}
-
-function showGolf() {
-    hideElements();
-    document.getElementById("golf_div").style.display="inline";
-}
-
-function showProjects() {
-    hideElements();
-    document.getElementById("projects").style.display="inline";
-}
-
-function showResume() {
-    //not already clicked
-    hideElements();
-    $("#resumeModal").modal('toggle');
-}
-
-function showContact() {
-    hideElements();
-    document.getElementById("contact_div").style.display="inline";
-}
-
 $( document ).ready(function() {
+    var width = $( window ).width();
+    var height = $( window ).height();
+    var max = Math.max(width, height);
+    $("#name").css("font-size",(max/20)+'px');
     if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        document.getElementById("desk").style.display="none";
-        document.getElementById("about-me").style.display="block";
-        document.getElementById("golf_div").style.display="block";
-        document.getElementById("projects").style.display="block";
-        document.getElementById("resume_div").style.display="block";
-        document.getElementById("contact_div").style.display="block";
-
-        //change CSS
-        $("#name").css("font-size",100+'px');
-        $(".title").css("font-size",60+'px');
-        $(".section-header").css("font-size",80+'px');
-        $(".description").css("font-size",28+'px');
-        $("#about-me-p").css("font-size",28+'px');
-        $("ul").css("font-size",28+'px');
+        //is on mobile device
+    } else {
+        drawSidebar(width, height);
     }
 });
